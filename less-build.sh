fba24494dd260b9c38e3a209d53503d5e6f02ba8
Aleksander Machniak <machniak@kolabsys.com>
2021-02-01 08:23:07 +0100

    Elastic dark mode
    Reviewers: #roundcube_kolab_plugins_developers
    
    Subscribers: #roundcube_kolab_plugins_developers
    
    Differential Revision: https://git.kolab.org/D2179

--- less-build.sh
+++ less-build.sh
@@ -3,7 +3,4 @@
 # First you have to link/copy /skins directory from Roundcube repo
 # into ./skins here
 
-# Note: You can remove -x option to generate non-minified file
-#       (remember to remove ".min" from the output file name)
-
-lessc --relative-urls -x plugins/libkolab/skins/elastic/libkolab.less > plugins/libkolab/skins/elastic/libkolab.min.css
+lessc --clean-css="--s1 --advanced" --rewrite-urls=all plugins/libkolab/skins/elastic/libkolab.less > plugins/libkolab/skins/elastic/libkolab.min.css
