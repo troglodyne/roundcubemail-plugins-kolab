5fc740dfda92e178eef5431bb441505be1a4021e
Andy Baugh <thomas.baugh@cpanel.net>
2024-01-03 09:50:32 -0600

    Make initial schema application forgive already being applied

--- plugins/calendar/drivers/caldav/SQL/sqlite.initial.sql
+++ plugins/calendar/drivers/caldav/SQL/sqlite.initial.sql
@@ -1,4 +1,4 @@
-CREATE TABLE kolab_alarms (
+CREATE TABLE IF NOT EXISTS kolab_alarms (
   alarm_id VARCHAR(255) NOT NULL,
   user_id INTEGER NOT NULL,
   notifyat DATETIME DEFAULT NULL,
@@ -6,9 +6,9 @@ CREATE TABLE kolab_alarms (
   PRIMARY KEY(alarm_id,user_id)
 );
 
-CREATE INDEX ix_kolab_alarms_user_id ON kolab_alarms(user_id);
+CREATE INDEX IF NOT EXISTS ix_kolab_alarms_user_id ON kolab_alarms(user_id);
 
-CREATE TABLE itipinvitations (
+CREATE TABLE IF NOT EXISTS itipinvitations (
   token VARCHAR(64) NOT NULL PRIMARY KEY,
   event_uid VARCHAR(255) NOT NULL,
   user_id INTEGER NOT NULL DEFAULT '0',
@@ -17,6 +17,6 @@ CREATE TABLE itipinvitations (
   cancelled TINYINT(3) NOT NULL DEFAULT '0'
 );
 
-CREATE INDEX ix_itipinvitations_uid ON itipinvitations(event_uid,user_id);
+CREATE INDEX IF NOT EXISTS ix_itipinvitations_uid ON itipinvitations(event_uid,user_id);
 
-INSERT INTO system (name, value) VALUES ('calendar-caldav-version', '2014041700');
+INSERT OR REPLACE INTO system (name, value) VALUES ('calendar-caldav-version', '2014041700');
